aaea0de964424753ba8e375e35f52363cf03b021
Minsun Kim <minsun2767@naver.com>
2016-10-18 20:16:50 +0900

    #5 Add ScrollView

--- app/src/main/java/kr/hs/emrim/minsun2767/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/simplediary/MainActivity.java
@@ -1,28 +1,24 @@
 package kr.hs.emrim.minsun2767.simplediary;
 
 import android.content.Context;
-import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     DatePicker datePic;
     EditText editDiary;
     Button butSave;
     String fileName;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,26 +27,22 @@ public class MainActivity extends AppCompatActivity {
         editDiary=(EditText)findViewById(R.id.edit_content);
         butSave=(Button)findViewById(R.id.but_save);
 
-
-        //현재 날짜 구하기
+//        현재 날짜 구하기
         Calendar calendar=Calendar.getInstance();
         int nowYear=calendar.get(Calendar.YEAR);
-        int month=calendar.get(Calendar.MONTH)+1;
+        final int month=calendar.get(Calendar.MONTH)+1; //0은 1월을 반환하므로 +1을 해야한다.
         int date=calendar.get(Calendar.DATE);
 
-        //DatePicker에 현재 날짜 설정
+//        DatePicker에 현재 날짜 설정
         datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 fileName=year+"_"+(monthOfYear+1)+"_"+dayOfMonth+".txt";
                 String content=readDiary(fileName);
                 editDiary.setText(content);
-                butSave.setEnabled(true);
-
-
+                butSave.setEnabled(true); //저장할 수 있게끔 활성화시킴
             }
         });
-
         butSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,32 +51,28 @@ public class MainActivity extends AppCompatActivity {
                     String diaryContents=editDiary.getText().toString();
                     out.write(diaryContents.getBytes());
                     out.close();
-                    Toast.makeText(getApplicationContext(),"저장이 완료됨",Toast.LENGTH_SHORT).show();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "저장이 완료됨", Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
-
     }
-    String readDiary(String fileName){
+    String readDiary(String fileName) {
         String diaryContents=null;
         try {
             FileInputStream in=openFileInput(fileName);
-            byte[] txt =new byte[500];
+            byte[] txt=new byte[500];
             in.read(txt);
             in.close();
             diaryContents=new String(txt);
             butSave.setText("수정하기");
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             editDiary.setHint("읽어올 일기가 없음");
             butSave.setText("새로 저장");
         }
+
         return diaryContents;
     }
 }
